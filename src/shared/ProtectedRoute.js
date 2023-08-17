b6064207c7cd89a1e22acf3b24e5a5a938c1d175
yigittekelioglu <yigittekelioglu@hotmail.com>
2023-08-18 00:42:28 +0300

    protected route

--- src/shared/ProtectedRoute.js
+++ src/shared/ProtectedRoute.js
@@ -10,15 +10,17 @@ const ProtectedRoute = ({component: Component, role, ...rest}) => {
         <Route
             {...rest}
             render={(props) => {
-                if (!currentUserRole) {
+                if (!currentUserRole) 
+                {
                     return <Redirect to="/login" />;
                 }
 
-                // Eğer kullanıcının rolü 'ADMIN' veya belirtilen role eşitse bu kontrolü geç
+                
                 if (currentUserRole.name === 'ADMIN' || currentUserRole.name === role) {
                     return <Component {...props} />;
-                } else {
-                    context.onLogoutSuccess();  // Kullanıcının oturumunu kapat
+                } 
+                else {
+                    context.onLogoutSuccess();  
                     return <Redirect to="/login" />;
                 }
                 
