3bc5b8c3585dcc9c94dc414c6f5f3ea4f02952fd
yigittekelioglu <yigittekelioglu@hotmail.com>
2023-09-08 03:08:25 +0300

    Frontend geliştirmeleri ve eklemeleri

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render() {
         return (
-            <div className='container'>
-                Home Page
+            <div className='container text-center mt-5 '>
+                <p className='font-weight-bold'>Jforce Bilgi Kayıt ve Yönetim Sistemine Hoşgeldiniz!</p>
             </div>
         );
     }
